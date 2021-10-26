6daa9d64995ccdbf8ac1ce81db70ef8a4d3c3728
JRamos29 <malebolge@gmail.com>
2021-10-26 19:45:34 -0300

    Implemented answer transition animation.

--- components/Answer.tsx
+++ components/Answer.tsx
@@ -11,34 +11,32 @@ interface AnswerProps {
 
 export default function Answer(props: AnswerProps) {
   const answer = props.value;
+  const revealedAnswer = answer.isRevealed ? styles.revealedAnswer : '';
 
   return (
     <div className={styles.answer} onClick={() => props.onAnswer(props.index)}>
-      <div className={styles.answerContent}>
-        {!answer.isRevealed ? (
-          <div className={styles.front}>
-            <div
-              className={styles.option}
-              style={{ backgroundColor: props.optionBgColor }}>
-              {props.option}
+      <div className={`${revealedAnswer} ${styles.answerContent}`}>
+        <div className={styles.front}>
+          <div
+            className={styles.option}
+            style={{ backgroundColor: props.optionBgColor }}>
+            {props.option}
+          </div>
+          <div className={styles.value}>{answer.value}</div>
+        </div>
+        <div className={styles.back}>
+          {answer.isRightAnswer ? (
+            <div className={styles.isRight}>
+              <div>The right answer is...</div>
+              <div className={styles.value}>{answer.value}</div>
             </div>
-            <div className={styles.value}>{answer.value}</div>
-          </div>
-        ) : (
-          <div className={styles.back}>
-            {answer.isRightAnswer ? (
-              <div className={styles.isRight}>
-                <div>The right answer is...</div>
-                <div className={styles.value}>{answer.value}</div>
-              </div>
-            ) : (
-              <div className={styles.isWrong}>
-                <div>The given answer is wrong...</div>
-                <div className={styles.value}>{answer.value}</div>
-              </div>
-            )}
-          </div>
-        )}
+          ) : (
+            <div className={styles.isWrong}>
+              <div>The given answer is wrong...</div>
+              <div className={styles.value}>{answer.value}</div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
