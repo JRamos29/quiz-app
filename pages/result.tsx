8f32c7959d70ee1df954ca341afefa95bd630384
JRamos29 <malebolge@gmail.com>
2021-10-26 19:05:52 -0300

    Modified result.tsx component.

--- pages/result.tsx
+++ pages/result.tsx
@@ -1,7 +1,19 @@
+import styles from '../styles/Result.module.css';
+import { useRouter } from 'next/router';
+
 export default function result() {
+  const router = useRouter();
+
+  const totalQuestions = +router.query.totalQuestions;
+  const totalRight = +router.query.totalRight;
+  const percentage = Math.round((totalRight / totalQuestions) * 100);
+
   return (
-    <div>
-      <h1>Result</h1>
+    <div className={styles.result}>
+      <h1>Final Score</h1>
+      <div>{totalQuestions}</div>
+      <div>{totalRight}</div>
+      <div>{`${percentage}%`}</div>
     </div>
   );
 }
